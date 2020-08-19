bbb627ff6dd0cefbb48545d8d273f3ec4e85efd4
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-19 18:45:03 +0500

    added initial pages

---
+++ src/pages/Done.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const  Done = () => {
+    return (
+        <div>
+            Hello Done
+        </div>
+    );
+};
+
+export default Done;
---
+++ src/pages/NotDone.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const  NotDone = () => {
+    return (
+        <div className="App">
+            Hello NotDone
+        </div>
+    );
+};
+
+export default NotDone;
