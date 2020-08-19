c5fd6bce34694621c046cdacf4c87755eafab397
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-19 18:53:35 +0500

    added Input Component

---
+++ src/components/Input/InputComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//styling
+import {Input} from "./InputStyle";
+
+const CustomInput = (props) => {
+
+    return(
+        <div>
+            Custom Input
+            <Input type='text'/>
+        </div>
+    )
+};
+
+export default CustomInput;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+//components
+import CustomInput from "../components/Input/InputComponent";
+
 const  Home = () => {
     return (
         <div>
             Hello
+            <CustomInput/>
         </div>
     );
 };
